ebe262fdf9ad7ee3918019d5085723157091a6bc
dcondo10 <dcondo@libelulasoft.com>
2020-07-26 16:21:27 -0500

    plugin para manejo de createdAt y updatedAt

--- src/models/Auth.models.js
+++ src/models/Auth.models.js
@@ -1,91 +1,93 @@
 import mongoose from 'mongoose';
+import timestamps from 'mongoose-timestamp';
 import { createUpdateProps } from './BaseObjecProps';
 
 const AplicacionSchema = mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  descripcion: {
-    type: String,
-    trim: true,
-  },
-  ...createUpdateProps,
+    nombre: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    descripcion: {
+        type: String,
+        trim: true,
+    },
 });
 
+AplicacionSchema.plugin(timestamps);
+AplicacionSchema.index({ createdAt: 1, updatedAt: 1 });
 export const Aplicacion = mongoose.model('Aplicaciones', AplicacionSchema);
 
 const PermisoSchema = mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+    nombre: {
+        type: String,
+        required: true,
+        trim: true,
+    },
 
-  descripcion: {
-    type: String,
-    trim: true,
-  },
+    descripcion: {
+        type: String,
+        trim: true,
+    },
 
-  aplicacion: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Aplicaciones',
-  },
-  ...createUpdateProps,
+    aplicacion: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Aplicaciones',
+    },
+    ...createUpdateProps,
 });
 
 export const Permiso = mongoose.model('Permisos', PermisoSchema);
 
 const RolSchema = mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-
-  descripcion: {
-    type: String,
-    trim: true,
-  },
-
-  permisos: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: 'Permisos',
+    nombre: {
+        type: String,
+        required: true,
+        trim: true,
     },
-  ],
-  ...createUpdateProps,
+
+    descripcion: {
+        type: String,
+        trim: true,
+    },
+
+    permisos: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Permisos',
+        },
+    ],
+    ...createUpdateProps,
 });
 
 export const Rol = mongoose.model('Roles', RolSchema);
 
 const UsuarioSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
 
-  roles: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: 'Roles',
-    },
-  ],
-  permisos: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: 'Permisos',
-    },
-  ],
-  ...createUpdateProps,
+    roles: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Roles',
+        },
+    ],
+    permisos: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Permisos',
+        },
+    ],
+    ...createUpdateProps,
 });
 
 export const Usuario = mongoose.model('Usuarios', UsuarioSchema);
